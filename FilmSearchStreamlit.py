9f0be5625d28288c44d303efa0edb06c493176ba
EdIzaguirre <eizaguir@berkeley.edu>
2024-04-05 17:01:04 -0700

    Added link to draft article

--- FilmSearchStreamlit.py
+++ FilmSearchStreamlit.py
@@ -27,6 +27,8 @@ films from the years {beginning_year}-{ending_year}. It will only recommend film
 database.
 """
 
+"If you are interested in details on how this was implemented, check out [this article](https://medium.com/@ed.izaguirre/16b4fa23e9ad)."
+
 
 def generate_response(input_text):
     chat = FilmSearch()
